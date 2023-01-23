2e4434e7f0a9cc1b2fbe2c8863909e4ed4b58aa7
DZZT2004 <taodaiuwu@gmail.com>
2023-01-23 23:41:48 +0100

    first commit

---
+++ database/migrations/2023_01_23_212653_create_fumetti_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFumettiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fumetti', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('description', 200);
+            $table->string('thumb', 200);
+            $table->mediumInteger('price');
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fumetti');
+    }
+}
